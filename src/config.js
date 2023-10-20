fb0284572602ec31f9516de16287f9b33b595540
tokebe <43009413+tokebe@users.noreply.github.com>
2023-10-20 13:07:36 -0400

    style: prettier formatting

--- src/config.js
+++ src/config.js
@@ -1,34 +1,36 @@
 exports.ONTOLOGIES = {
-  'doid': {
-    'name': 'doid',
-    'url': 'http://purl.obolibrary.org/obo/doid.owl'
+  doid: {
+    name: "doid",
+    url: "http://purl.obolibrary.org/obo/doid.owl",
   },
-  'go': {
-    'name': 'go',
-    'url': 'http://purl.obolibrary.org/obo/go.owl'
-  }, 
-  'mondo': {
-    'name': 'mondo',
-    'url': 'http://purl.obolibrary.org/obo/mondo.owl'
+  go: {
+    name: "go",
+    url: "http://purl.obolibrary.org/obo/go.owl",
   },
-  'hp': {
-    'name': 'hp',
-    'url': 'http://purl.obolibrary.org/obo/hp.owl'
+  mondo: {
+    name: "mondo",
+    url: "http://purl.obolibrary.org/obo/mondo.owl",
   },
-  'chebi': { // not sure why but downloading chebi.owl using curl or node js fails
-    'name': 'chebi',
-    'url': 'https://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.owl'
-  }, 
-  'umls': { // manually processed from MRREL.REF file in UMLS dump
-    'name': 'umls'
-  }
-}
+  hp: {
+    name: "hp",
+    url: "http://purl.obolibrary.org/obo/hp.owl",
+  },
+  chebi: {
+    // not sure why but downloading chebi.owl using curl or node js fails
+    name: "chebi",
+    url: "https://ftp.ebi.ac.uk/pub/databases/chebi/ontology/chebi.owl",
+  },
+  umls: {
+    // manually processed from MRREL.REF file in UMLS dump
+    name: "umls",
+  },
+};
 
 exports.CHILD_RELATIONS = {
-  'is_a': true,
+  is_a: true,
   // 'http://purl.obolibrary.org/obo/BFO_0000050': true, // part of
-}
+};
 
 exports.PARENT_RELATIONS = {
-  'http://purl.obolibrary.org/obo/BFO_0000051': true, // has part
-}
+  "http://purl.obolibrary.org/obo/BFO_0000051": true, // has part
+};
--- src/download.js
+++ src/download.js
@@ -1,7 +1,7 @@
 //find and download and parse
-const { ONTOLOGIES } = require('./config');
-const { parseFile } = require('./parser');
-const child_process = require('child_process');
+const { ONTOLOGIES } = require("./config");
+const { parseFile } = require("./parser");
+const child_process = require("child_process");
 
 //iterate over ontologies obj
 for (let ontology of Object.values(ONTOLOGIES)) {
@@ -11,21 +11,22 @@ for (let ontology of Object.values(ONTOLOGIES)) {
   let download_process = child_process.exec(`curl -o ../data/${ontology.name}.owl -L ${url}`);
   download_process.stdout.pipe(process.stdout);
   download_process.stderr.pipe(process.stderr);
-  
-  download_process.on('close', (code) => {
+
+  download_process.on("close", code => {
     if (code !== 0) {
       console.log(`Error downloading ${url}`);
       return;
     }
     console.log(`Done downloading ${url} with code ${code}`);
-    
+
     // use robot to convert owl to json
-    child_process.execSync(`../robot convert -i ../data/${ontology.name}.owl --format json -o ../data/${ontology.name}.json`);
+    child_process.execSync(
+      `../robot convert -i ../data/${ontology.name}.owl --format json -o ../data/${ontology.name}.json`,
+    );
     console.log(`Parsing ${url}`);
     parseFile(`../data/${ontology.name}.json`);
     console.log(`Cleaning up ${url}`);
     child_process.exec(`rm ../data/${ontology.name}.owl`);
     child_process.exec(`rm ../data/${ontology.name}.json`);
   });
-
 }
